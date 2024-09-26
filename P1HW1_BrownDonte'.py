478b22d54ce260ac0d2c639d88e2df9a0be0613d
Marshic <dontebrojr0816@gmail.com>
2024-09-26 14:05:31 -0400

    Add files via upload

---
+++ P1HW1_BrownDonte'.py
@@ -0,0 +1,45 @@
+# Name
+# 9/24/2024
+# P1LAB1
+# Get input from the user and calculate math calculations
+
+print("---Calculating Exponents---")
+print()
+print()
+
+# Get info from user
+base_value = float(input("Enter an integer as the base value: "))
+exponent = float(input("Enter an integer as the exponent: "))
+print()
+print()
+
+# Calculate the value of the exponent math
+result = base_value ** exponent
+
+# Display results of the math
+print (base_value, "raised to the power of", exponent, "is", result, "!!")
+print()
+print()
+
+# Addition and Subtraction Section
+print("----Addition and Subtraction----")
+print()
+print()
+
+start_int = float(input("Enter a starting integer: "))
+add_int = float(input("Enter an integer to add: "))
+sub_int = float(input("Enter an integer to subtract: "))
+print()
+print()
+
+# Calculate math
+result2 = start_int + add_int - sub_int
+
+# Display results of the math
+print (start_int, "+", add_int, "-", sub_int, "is equal to", result2, "!!")
+
+
+       
+
+
+       
