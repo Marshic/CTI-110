2b159875e8c5f94955ea9fba30e5998080ee9222
Marshic <dontebrojr0816@gmail.com>
2024-10-03 13:25:26 -0400

    Add files via upload

---
+++ mathconcepts.py
@@ -0,0 +1,15 @@
+# Donte' Brown
+# October 3rd
+# Pythagorean Calculator CTI-110-5H01
+
+# Get input for the values of the sides from the user.
+side1 = float(input("Enter value for side one: "))
+side2 = float(input("Enter value for side two: "))
+print ("-*-" * 20)
+
+# Values
+hypotenuse = (side1 ** 2) + (side2 ** 2)
+
+
+# Display to user
+print (f"A hypotenuse with the sides {side1} and {side2} is equal to {hypotenuse}")
