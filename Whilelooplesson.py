2873bda4956aae2c734279d542058cc95c16eb1c
Marshic <dontebrojr0816@gmail.com>
2024-11-07 14:07:06 -0500

    Add files via upload

---
+++ Whilelooplesson.py
@@ -0,0 +1,16 @@
+# While loop examples
+'''
+run_again = input("Enter 'run' to run the program or 'exit' to end: ")
+
+while run_again.lower() != "exit":
+    print("Do something...")
+    run_again = input("Enter 'yes' to run or 'exit' to end: ")
+# Loop breaks here
+print("Thanks for using the program")
+'''
+num = 0
+
+while num < 5:
+    print(u"\U0001F346" * 10)
+    num += 1
+print("Loop has ended!")
